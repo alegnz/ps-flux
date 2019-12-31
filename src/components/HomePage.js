446a1104d67f028d38b41a662558e088da838e88
alegnz <gnzalejandro@gmail.com>
2019-12-31 11:13:58 -0300

    Add first pages

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage(props) {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight administration</h1>
+      <p>React, Flux and React Router for ultra-responsive web apps.</p>
+    </div>
+  );
+}
+
+export default HomePage;
