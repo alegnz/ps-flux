a8c3852278de48f94e572a72e8fa2fe8a4f96c2e
alegnz <gnzalejandro@gmail.com>
2020-01-04 16:52:23 -0300

    Add course form

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import CoursesPage from "./CoursesPage";
 import NotFoundPage from "./NotFoudPage";
 import ManagecoursePage from "./ManageCoursePage";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManagecoursePage} />
+        <Route path="/course/" component={ManagecoursePage} />
 
         <Redirect from="/about-page" to="about" />
 
---
+++ src/components/CourseForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+import TextInput from "./common/TextInput";
+import PropTypes from "prop-types";
+
+function CourseForm(props) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <TextInput
+        id="title"
+        name="title"
+        label="Title"
+        error={props.errors.title}
+        value={props.course.title}
+        onChange={props.onChange}
+      />
+
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            onChange={props.onChange}
+            value={props.course.authorId || ""}
+            className="form-control"
+          >
+            <option value="" />
+            <option value="1">Cory House</option>
+            <option value="2">Scott Allen</option>
+          </select>
+        </div>
+        {props.errors.author && (
+          <div className="alert alert-danger">{props.errors.author}</div>
+        )}
+      </div>
+
+      <TextInput
+        id="category"
+        name="category"
+        label="Category"
+        error={props.errors.category}
+        value={props.course.category}
+        onChange={props.onChange}
+      />
+
+      <input type="submit" value="Save" className="btn btn-primary" />
+    </form>
+  );
+}
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onchange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+export default CourseForm;
